4d0f652b4215ca89eeb75e7443c68fdfaf99f96b
Alejo Fernandez <alejo.fernandez@gmail.com>
2013-12-04 17:45:39 -0300

    Fixed first/last name issue

--- web.js
+++ web.js
@@ -1,16 +1,20 @@
 var express = require("express");
 var app = express();
+
 app.use(express.logger());
 
 app.get('/get-vcard', function(request, response) {
+  var fullName = request.query.name || '';
+  var firstName = fullName.substr(0, fullName.indexOf(' '));
+  var lastName = fullName.substr(fullName.indexOf(' ') + 1);
+  var phone = '+' + (request.query.phone || '');
   var card = '' +
     'BEGIN:VCARD\n' +
     'VERSION:2.1\n' +
-    'N:' + (request.query.name || '') + ';\n' +
-    'FN:\n' +
-    'ORG:\n' +
-    'TEL;WORK;VOICE;PREF:+' + (request.query.phone || '') + '\n' +
-    'REV:20110412165200\n' +
+    'N:' + lastName + ';' + firstName + '\n' +
+    'FN:' + fullName + '\n' +
+    'ORG:PayFriendz\n' +
+    'TEL;CELL;VOICE;PREF:' + phone + '\n' +
     'END:VCARD';
 
   response.setHeader('Content-disposition', 'inline; filename=contact.vcf');
